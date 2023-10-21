2fa3c6c7bdba806a69a750e87f7461baff9589c6
Jossua Figueiras <jossufigueiras@icloud.com>
2023-10-21 17:36:44 +0200

    Ajout du constructeur de point, ajout de la liste de point qui contient autant de point que l'entier précisé dans une constante

--- Main.py
+++ Main.py
@@ -1,11 +1,17 @@
 import turtle
 
+NB_POINT = 12
+
 class Point:
-    def __init__(self, x, y, dot, color):
-        self.x = x
-        self.y = y
+    def __init__(self, dot, color, x = None, y = None):
         self.dot = dot
         self.color = color
+        self.x = x
+        self.y = y
+    
+    def Draw_Point(Point):
+        pen.goto(Point.x, Point.y)
+        pen.dot(Point.dot)
 
 def window_configu():
     window = turtle.Screen()
@@ -13,13 +19,23 @@ def window_configu():
     window.bgcolor("#555")
     return window
 
+def Create_Point_List():
+    """Create a point list without value for x and y"""
+    point_list = []
+    for i in range(NB_POINT):
+        point_list.append(Point(10, "red"))
+    return point_list
+
+def Point_axes(point_list):
+    """Return point list with x and y value"""
+
+
+
 #def loading_bar_points():
 
 window = window_configu()
-
-
+turtle.hideturtle()
 pen = turtle.Turtle()
-pen.penup()
-pen.goto(0, 0)
-pen.dot(10)
+turtle.penup()
+Point.Draw_Point(Point(30, "red", 0, 50 ))
 turtle.done()
